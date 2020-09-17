ab1e3634af9869220fd731941478c2bae4fb019e
tedisy47123 <tedisya47@gmail.com>
2020-09-17 16:20:16 +0700

    update url

--- movie_teather/urls.py
+++ movie_teather/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+
+from django.conf import settings
+from django.views.static import serve
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('',views.index),
     path('', include('film.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,})
 ]
